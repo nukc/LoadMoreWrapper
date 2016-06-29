b3df557dd7674136d6e869f6da5ddfec9745248d
nukc <353932158@qq.com>
2016-06-29 16:45:58 +0800

    Initial commit

---
+++ app/src/main/java/com/github/nukc/sample/SampleFragment.java
@@ -0,0 +1,146 @@
+package com.github.nukc.sample;
+
+
+import android.os.Bundle;
+import android.support.annotation.BoolRes;
+import android.support.annotation.IntDef;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.github.nukc.recycleradapter.RecyclerAdapter;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SampleFragment extends Fragment {
+
+    private static final String ARGS_COUNT = "count";
+    private static final String ARGS_MANAGER_MODE = "managerMode";
+
+    public static final int MODE_LINEARLAYOUT = 1;
+    public static final int MODE_GRIDLAYOUT = 2;
+    public static final int MODE_STAGGEREDGRIDLAYOUT = 3;
+
+    @IntDef({MODE_LINEARLAYOUT, MODE_GRIDLAYOUT, MODE_STAGGEREDGRIDLAYOUT})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface ManagerMode{}
+
+    private SampleAdapter mSampleAdapter;
+
+    public static SampleFragment newInstance(int count, @ManagerMode int managerMode) {
+        Bundle args = new Bundle();
+        args.putInt(ARGS_COUNT, count);
+        args.putInt(ARGS_MANAGER_MODE, managerMode);
+
+        SampleFragment fragment = new SampleFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public SampleFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        int count = getArguments().getInt(ARGS_COUNT, 0);
+        mSampleAdapter = new SampleAdapter(count);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sample, container, false);
+
+        final RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        int managerMode = getArguments().getInt(ARGS_MANAGER_MODE);
+
+        switch (managerMode) {
+            case MODE_LINEARLAYOUT:
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(container.getContext());
+                recyclerView.setLayoutManager(linearLayoutManager);
+                break;
+            case MODE_GRIDLAYOUT:
+                GridLayoutManager gridLayoutManager = new GridLayoutManager(container.getContext(), 2);
+                recyclerView.setLayoutManager(gridLayoutManager);
+                break;
+            case MODE_STAGGEREDGRIDLAYOUT:
+                StaggeredGridLayoutManager staggeredGridLayoutManager =
+                        new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
+                recyclerView.setLayoutManager(staggeredGridLayoutManager);
+                break;
+            default:
+                throw new IllegalArgumentException();
+        }
+
+        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(mSampleAdapter);
+        recyclerView.setAdapter(recyclerAdapter);
+        recyclerAdapter.setLoadMoreListener(new RecyclerAdapter.OnLoadMoreListener() {
+            @Override
+            public void onLoadMore() {
+                recyclerView.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mSampleAdapter.addItem();
+                    }
+                }, 1500);
+            }
+        });
+
+        return view;
+    }
+
+
+    private static class SampleAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+        private int mCount;
+
+        public SampleAdapter(int count) {
+            mCount = count;
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_sample, parent, false);
+            return new SampleHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            ((SampleHolder) holder).mTextView.setText(position + "");
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCount;
+        }
+
+        public void addItem() {
+            mCount+= 5;
+            notifyDataSetChanged();
+        }
+
+        static class SampleHolder extends RecyclerView.ViewHolder {
+            TextView mTextView;
+
+            public SampleHolder(View itemView) {
+                super(itemView);
+                mTextView = (TextView) itemView.findViewById(R.id.text);
+            }
+        }
+    }
+}
---
+++ recycleradapter/src/main/java/com/github/nukc/recycleradapter/RecyclerAdapter.java
@@ -0,0 +1,237 @@
+package com.github.nukc.recycleradapter;
+
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by C on 16/6/27.
+ */
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+    private static final String TAG = RecyclerAdapter.class.getSimpleName();
+    private static final byte TYPE_FOOTER = -2;
+
+    private RecyclerView.Adapter mAdapter;
+    private View mFooterView;
+    private int mFooterResId = View.NO_ID;
+
+    private RecyclerView mRecyclerView;
+    private OnLoadMoreListener mOnLoadMoreListener;
+
+    public RecyclerAdapter(@NonNull RecyclerView.Adapter adapter) {
+        registerAdapter(adapter);
+    }
+
+    public RecyclerAdapter(@NonNull RecyclerView.Adapter adapter, View footerView) {
+        registerAdapter(adapter);
+        mFooterView = footerView;
+    }
+
+    public RecyclerAdapter(@NonNull RecyclerView.Adapter adapter, @LayoutRes int resId) {
+        registerAdapter(adapter);
+        mFooterResId = resId;
+    }
+
+    private void registerAdapter(RecyclerView.Adapter adapter) {
+        if (adapter == null) {
+            throw new NullPointerException("adapter can not be null!");
+        }
+
+        mAdapter = adapter;
+        mAdapter.registerAdapterDataObserver(mObserver);
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Log.i(TAG, "create");
+        if (viewType == TYPE_FOOTER) {
+            if (mFooterResId != View.NO_ID) {
+                mFooterView = LayoutInflater.from(parent.getContext()).inflate(mFooterResId, parent, false);
+            }
+            if (mFooterView != null) {
+                return new FooterHolder(mFooterView);
+            }
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.base_footer, parent, false);
+            return new FooterHolder(view);
+        }
+
+        return mAdapter.onCreateViewHolder(parent, viewType);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        Log.i(TAG, "bind");
+
+        if (holder instanceof FooterHolder) {
+            if (!canScroll()) {
+                holder.itemView.setVisibility(View.GONE);
+            }
+        } else {
+            mAdapter.onBindViewHolder(holder, position);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        int count = mAdapter.getItemCount();
+        return count == 0 ? 0 : count + 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == mAdapter.getItemCount()) {
+            return TYPE_FOOTER;
+        }
+        return super.getItemViewType(position);
+    }
+
+    public boolean canScroll() {
+        return ViewCompat.canScrollVertically(mRecyclerView, -1);
+    }
+
+    static class FooterHolder extends RecyclerView.ViewHolder {
+
+        public FooterHolder(View itemView) {
+            super(itemView);
+
+            ViewGroup.LayoutParams layoutParams = itemView.getLayoutParams();
+            if (layoutParams instanceof StaggeredGridLayoutManager.LayoutParams) {
+                ((StaggeredGridLayoutManager.LayoutParams) layoutParams).setFullSpan(true);
+            }
+        }
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        Log.i(TAG, "attached");
+        mRecyclerView = recyclerView;
+        recyclerView.addOnScrollListener(mOnScrollListener);
+
+        RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+        if (layoutManager instanceof GridLayoutManager) {
+            final GridLayoutManager gridLayoutManager = ((GridLayoutManager) layoutManager);
+            final GridLayoutManager.SpanSizeLookup originalSizeLookup = gridLayoutManager.getSpanSizeLookup();
+
+            gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+                @Override
+                public int getSpanSize(int position) {
+                    if (getItemViewType(position) == TYPE_FOOTER) {
+                        return gridLayoutManager.getSpanCount();
+                    } else if (originalSizeLookup != null) {
+                        return originalSizeLookup.getSpanSize(position);
+                    }
+
+                    return 1;
+                }
+            });
+        }
+    }
+
+    private RecyclerView.OnScrollListener mOnScrollListener = new RecyclerView.OnScrollListener() {
+        @Override
+        public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+            super.onScrollStateChanged(recyclerView, newState);
+
+            if (!ViewCompat.canScrollVertically(recyclerView, -1)) {
+                Log.d(TAG, "recyclerView can not scroll!");
+                return;
+            }
+
+            if (newState == RecyclerView.SCROLL_STATE_IDLE && mOnLoadMoreListener != null) {
+                boolean isBottom;
+                RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+                if (layoutManager instanceof LinearLayoutManager) {
+                    isBottom = ((LinearLayoutManager) layoutManager).findLastVisibleItemPosition()
+                            >= layoutManager.getItemCount() - 1;
+                } else if (layoutManager instanceof StaggeredGridLayoutManager) {
+                    StaggeredGridLayoutManager sgLayoutManager = (StaggeredGridLayoutManager) layoutManager;
+                    int[] into = new int[sgLayoutManager.getSpanCount()];
+                    sgLayoutManager.findLastVisibleItemPositions(into);
+
+                    isBottom = last(into) >= layoutManager.getItemCount() - 1;
+                }else {
+                    isBottom = ((GridLayoutManager) layoutManager).findLastVisibleItemPosition()
+                            >= layoutManager.getItemCount() - 1;
+                }
+
+                if (layoutManager.getItemCount() > 0 && isBottom) {
+                    mOnLoadMoreListener.onLoadMore();
+                }
+            }
+        }
+
+        @Override
+        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            super.onScrolled(recyclerView, dx, dy);
+        }
+    };
+
+    //取到最后的一个节点
+    private static int last(int[] lastPositions) {
+        int last = lastPositions[0];
+        for (int value : lastPositions) {
+            if (value > last) {
+                last = value;
+            }
+        }
+        return last;
+    }
+
+    @Override
+    public void onDetachedFromRecyclerView(RecyclerView recyclerView) {
+        recyclerView.removeOnScrollListener(mOnScrollListener);
+        mAdapter.unregisterAdapterDataObserver(mObserver);
+        mRecyclerView = null;
+    }
+
+
+    public void setLoadMoreListener(OnLoadMoreListener listener) {
+        mOnLoadMoreListener = listener;
+    }
+
+    public interface OnLoadMoreListener{
+        void onLoadMore();
+    }
+
+    private RecyclerView.AdapterDataObserver mObserver = new RecyclerView.AdapterDataObserver() {
+        @Override
+        public void onChanged() {
+            RecyclerAdapter.this.notifyDataSetChanged();
+        }
+
+        @Override
+        public void onItemRangeChanged(int positionStart, int itemCount) {
+            RecyclerAdapter.this.notifyItemRangeChanged(positionStart, itemCount);
+        }
+
+        @Override
+        public void onItemRangeChanged(int positionStart, int itemCount, Object payload) {
+            RecyclerAdapter.this.notifyItemRangeChanged(positionStart, itemCount, payload);
+        }
+
+        @Override
+        public void onItemRangeInserted(int positionStart, int itemCount) {
+            RecyclerAdapter.this.notifyItemRangeInserted(positionStart, itemCount);
+        }
+
+        @Override
+        public void onItemRangeRemoved(int positionStart, int itemCount) {
+            RecyclerAdapter.this.notifyItemRangeRemoved(positionStart, itemCount);
+        }
+
+        @Override
+        public void onItemRangeMoved(int fromPosition, int toPosition, int itemCount) {
+            RecyclerAdapter.this.notifyItemMoved(fromPosition, toPosition);
+        }
+    };
+}
